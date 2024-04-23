2aca3633cc7b2963f7d5e6d78cbbbf21ddcccfb6
vyomgangwar25 <vyomgangwar25@gmail.com>
2024-04-23 18:32:19 +0530

    create react app

---
+++ src/components/TaskInput.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const TaskInput = () => {
+    const[todo,setTodo]=useState("");
+
+    const handleChange=(e)=>{
+        setTodo(e.target.value);
+    }
+  //  console.log(todo)
+  return (
+    <div className='addTodos'>
+    <input type="text" onChange={handleChange} className='todo-input' />
+
+      <button className='add-btn'>Add</button>
+    </div>
+  )
+}
+
+export default TaskInput
