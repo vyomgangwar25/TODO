f54ee3800c5fd7ecc0ac079eb20e2aa65a0f3e3a
vyomgangwar25 <vyomgangwar25@gmail.com>
2024-04-24 15:31:37 +0530

    commit

--- src/Reducers/toDoSlider.js
+++ src/Reducers/toDoSlider.js
@@ -17,13 +17,10 @@ export const toDoSlider = createSlice({
       let { todoList } = state;
       state.todoList = todoList.filter((item) => item.id !== action.payload.id);
     },
-    // editTodo: (state, action) => {
-    //   let { todoList } = state;
-    //   state.todoList = todoList.map((item) => item.id === action.payload.id ? action.payload : item);
-    // }
+    
   },
 })
  
-export const { addToDo, deleteToDo, editTodo } = toDoSlider.actions
+export const { addToDo, deleteToDo } = toDoSlider.actions
 
 export default toDoSlider.reducer;
